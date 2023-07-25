a4207c7b19f08c650f46fa656a710237afedcc06
amirmarin <amir.marin@getpulpo.com>
2023-07-24 19:59:55 -0500

    login

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,24 +1,27 @@
 
 export default function Page() {
   return (<>
-  <form className="w-64 text-left">
+   <h1 className="text-white text-4xl font-bold mb-1">Login</h1>
+        <p className="text-white text-lg mb-12">¡Bienvenido!</p>
+  <form className="w-[30rem] text-left">
   <div className="mb-4">
-    <label className="block text-gray-700 text-sm font-bold mb-2" >Correo electrónico</label>
-    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Correo electrónico"/>
+    <label className="block text-white text-sm font-bold mb-2" >Correo electrónico</label>
+    <input className="shadow appearance-none border bg-[#5141EA] rounded-2xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email"/>
   </div>
   <div className="mb-6">
-    <label className="block text-gray-700 text-sm font-bold mb-2" >Contraseña</label>
-    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Contraseña"/>
+    <label className="block text-white text-sm font-bold mb-2" >Contraseña</label>
+    <input className="shadow appearance-none border bg-[#5141EA] rounded-2xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"/>
   </div>
-    <button className="bg-gradient-to-r from-blue-300  to-blue-600 text-white mx-4 px-6 py-1 rounded-3xl text-base">
+  <div className="flex my-4">
+            <input type="checkbox" className="h-5 w-5 text-purple-600 rounded mr-2" />
+            <label className="text-white text-sm" htmlFor="remember">
+              Recordarme
+            </label>
+          </div>
+    <button className="bg-gradient-to-r from-blue-300  to-blue-600 text-white mx-4 ml-0 px-6 py-1 rounded-3xl text-base">
         Crear Cuenta
     </button>
-  <div className="flex ">
  
-    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-      ¿Olvidaste tu contraseña?
-    </a>
-  </div>
 </form>
 </>)
 }
